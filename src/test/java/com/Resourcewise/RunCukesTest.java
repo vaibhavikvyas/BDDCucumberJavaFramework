f5c915115861eafe5d460e38bba5edb558b1bb3e
vaibhavikvyas <vaibhavikvyas@gmail.com>
2022-11-05 12:52:49 +0000

    @Registration test

--- src/test/java/com/Resourcewise/RunCukesTest.java
+++ src/test/java/com/Resourcewise/RunCukesTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features ="src/test/resources",
         dryRun = false,
         strict = true,
-        tags = "@Login",
+        tags = "@Registration",
         plugin = "json:target/cucumber-report/cucumber.json")
 public class RunCukesTest{
 // a json report is generated as target/cucumber-report
